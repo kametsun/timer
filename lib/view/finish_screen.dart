ea9a0b1464138b95fdc21f7f25c352f9b72a8cd7
kametsun <kametsuba.0713@gmail.com>
2023-07-17 18:27:00 +0900

    終了画面のUI変更

--- lib/view/finish_screen.dart
+++ lib/view/finish_screen.dart
@@ -9,23 +9,61 @@ class FinishScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text("お疲れ様でした"),
-            const Text("合計作業時間"),
-            Text(
-              "${(totalWorkTime ~/ 60).toString().padLeft(2, '0')}:${(totalWorkTime % 60).toString().padLeft(2, '0')}",
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width *
+                0.8, // Here we set width to 80% of the screen width
+            child: Card(
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "お疲れ様でした",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "合計作業時間",
+                      style: TextStyle(fontSize: 20, color: Colors.grey[700]),
+                    ),
+                    Text(
+                      "${(totalWorkTime ~/ 60).toString().padLeft(2, '0')}:${(totalWorkTime % 60).toString().padLeft(2, '0')}",
+                      style:
+                          TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 30),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.orange, // background
+                        onPrimary: Colors.white, // foreground
+                      ),
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const MyHomePage()));
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 30.0, vertical: 10.0),
+                        child: Text("戻る", style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) => const MyHomePage()));
-              },
-              child: const Text("戻る"),
-            ),
-          ],
+          ),
         ),
       ),
     );
